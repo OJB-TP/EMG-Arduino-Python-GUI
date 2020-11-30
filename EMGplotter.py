ab39996dcc45f4df59df0e669180f798bd319648
Andrea Zedda <andrea.zedda@outlook.it>
2020-11-30 18:32:33 +0100

    button link print
    I bottoni, quando premuti printano qualcosa

--- EMGplotter.py
+++ EMGplotter.py
@@ -9,6 +9,7 @@ from PyQt5 import QtWidgets
 from pyqtgraph import PlotWidget, plot
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QPushButton
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
@@ -18,13 +19,29 @@ class EMGApp(QtWidgets.QMainWindow, EMGreader.Ui_MainWindow):
     def __init__(self, parent=None):
         super(EMGApp, self).__init__(parent)
         self.setupUi(self)
-
+        
+def printStartBtn():
+    print("Start pressed")
+    
+def printStopBtn():
+    print ("Stop pressed")
+    
+def printConnectBtn():
+    print ("Connect pressed")
+    
+def printRecBtn():
+    print ("REC pressed")
+    
 def main():
     app = QApplication(sys.argv)
     form = EMGApp()
+    form.startButton.clicked.connect (printStartBtn)
+    form.stopButton.clicked.connect (printStopBtn)
+    form.connectButton.clicked.connect (printConnectBtn)
+    form.recButton.clicked.connect (printRecBtn)
+
     form.show()
     app.exec_()
-
 
 if __name__ == '__main__':
     main()
