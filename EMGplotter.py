6e0dfb04c25ce0f1b0a7ddde66e7b4c011c19514
Andrea Zedda <andrea.zedda@outlook.it>
2020-12-03 09:52:07 +0100

    loop inserted but not working properly
    trying to insert loop

--- EMGplotter.py
+++ EMGplotter.py
@@ -11,23 +11,35 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QPushButton
 import pyqtgraph as pg
-import sys  # We need sys so that we can pass argv to QApplication
+import sys
 import os
 import EMGreader
+import serial
+import time
+from guiLoop import guiLoop, stopLoop
+
+started = False
+arduino = serial.Serial('COM33', 9600, timeout=.1)
 
 class EMGApp(QtWidgets.QMainWindow, EMGreader.Ui_MainWindow):
     def __init__(self, parent=None):
         super(EMGApp, self).__init__(parent)
         self.setupUi(self)
         
+
+
 def printStartBtn():
     print("Start pressed")
+    started = True
+    StartLoop(EMGreader.Ui_MainWindow)
     
 def printStopBtn():
     print ("Stop pressed")
+    Stop
     
 def printConnectBtn():
     print ("Connect pressed")
+    arduino.open()
     
 def printRecBtn():
     print ("REC pressed")
@@ -42,6 +54,26 @@ def main():
 
     form.show()
     app.exec_()
+
+def Start():
+    if not started:
+        # you can also use threads here, see the first link
+        started = True
+        print (started)
+        
+def Stop():
+    if started:
+        #stopLoop(self.started)
+        started = False
+        print (started)
+
+@guiLoop
+def StartLoop():
+        # This is your Start function
+        # ...
+        print ("in startloop")
+        while True:
+            print (arduino.readline())    
 
 if __name__ == '__main__':
     main()
